5fc92990ab7beb49e21d823f1ca6362661a033b5
ErlinaNP <181116002@mhs.stiki.ac.id>
2022-03-29 22:34:00 +0700

    commiting

---
+++ components/Navbar.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <b-navbar
+      toggleable="lg"
+      type="light"
+      variant="white"
+      class="shadow padding-navbar"
+    >
+      <nuxt-link to="/">
+        <img src="logo.svg" alt="" srcset="" />
+      </nuxt-link>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="#"
+            ><nuxt-link to="/">Beranda</nuxt-link></b-nav-item
+          >
+          <b-nav-item href="#"
+            ><nuxt-link to="/product">Produk</nuxt-link></b-nav-item
+          >
+          <b-nav-item href="#"
+            ><nuxt-link to="/promo">Promo</nuxt-link></b-nav-item
+          >
+          <b-nav-item href="#"
+            ><nuxt-link to="/about-us">Tentang Kami</nuxt-link></b-nav-item
+          >
+          <b-nav-item href="#"
+            ><nuxt-link to="/contact">Hubungi Kami</nuxt-link></b-nav-item
+          >
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ms-auto">
+          <b-nav-item href="#"
+            ><i class="fa-solid fa-cart-shopping"></i
+          ></b-nav-item>
+          <b-nav-item-dropdown v-if="$auth.user" right>
+            <!-- Using 'button-content' slot -->
+            <template #button-content>
+              <em>{{$auth.user.name}}</em>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item @click="logout" href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+          <b-nav-item v-else href="#"
+            ><nuxt-link to="/login">Masuk</nuxt-link></b-nav-item
+          >
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+<style scoped>
+  a{
+    color: black;
+    text-decoration: none;
+  }
+</style>
+<script>
+export default {
+  methods:{
+    async logout(){
+      await this.$auth.logout()
+      location.reload();
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <div class="padding-navbar">
+      <img src="/banner/banner.png" class="w-100" />
+    </div>
+    <div class="container-fluid my-5 px-0 bg-light">
+      <div class="d-flex justify-content-between padding-navbar py-3">
+        <strong>Promo Terkini</strong>
+        <nuxt-link to="/promo">Promo Lainya</nuxt-link>
+      </div>
+      <div class="d-flex justify-content-around pb-5">
+        <img src="/promo/promo1.png" class="h-100" />
+        <img src="/promo/promo2.png" class="h-100" />
+        <img src="/promo/promo3.png" class="h-100" />
+      </div>
+    </div>
+    <div class="container-fluid my-5 px-0">
+      <div class="d-flex justify-content-between padding-navbar py-3">
+        <strong>Kategori</strong>
+      </div>
+      <div class="d-flex justify-content-between padding-navbar pb-3">
+        <button type="button" class="btn btn-light rounded-pill">Semua</button>
+        <button type="button" class="btn btn-light rounded-pill">Atasan</button>
+        <button type="button" class="btn btn-light rounded-pill">
+          Bawahan
+        </button>
+        <button type="button" class="btn btn-light rounded-pill">Hijab</button>
+        <button type="button" class="btn btn-light rounded-pill">
+          Aksesoris
+        </button>
+        <button type="button" class="btn btn-light rounded-pill">Dewasa</button>
+        <button type="button" class="btn btn-light rounded-pill">
+          Anak anak
+        </button>
+      </div>
+    </div>
+    <div class="row padding-navbar">
+      <div
+        class="col-lg-3 col-md-6 pb-5"
+        v-for="(item, index) in data"
+        :key="index"
+      >
+        <div class="card shadow">
+          <img :src="item.photo" />
+          <h6 class="px-3 pt-3">
+            {{ item.title }}
+          </h6>
+          <h6 class="p-3">
+            <strong>Rp. {{ item.original_price }}</strong>
+          </h6>
+          <button class="btn btn-danger" @click="bayar">Masukan Ke Keranjang</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      data: [],
+    }
+  },
+  methods: {
+    async fetch() {
+      await this.$axios.get('api/product').then((res) => {
+        this.data = res.data.data
+        console.log(this.data)
+      })
+    },
+  },
+  mounted() {
+    this.fetch()
+  },
+}
+</script>
